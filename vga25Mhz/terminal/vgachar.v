df051efcf9408658bba399e9d44f1c99a804462c
Bob Smith <bob@demandperipherals.org>
2023-04-06 21:58:14 -0700

    Revert "Rearranged vgachar.c to put allocations at top of file.  NO FUNCTIONAL CHANGES."
    This reverts commit 24bc0826381d7cbb703c00f3eb3b58b1d672e2ef.

--- vga25Mhz/terminal/vgachar.v
+++ vga25Mhz/terminal/vgachar.v
@@ -3,22 +3,22 @@
  *
  * This module is the guts of the terminal peripheral.  It consists of a VGA controller,
  * a display buffer, a font ROM, and logic to control the cursor and the character to be
- * written to the cursor position.  The VGA controller provides the current location of
- * the pixel being displayed.  The display buffer contains both the codes and
+ * written to the cursor position.  The VGA controller provides the current location of 
+ * the pixel being displayed.  The display buffer contains both the codes and 
  * attributes of the characters accessed by row and column. The character codes are
  * indeces to the font ROM that contains the shape of the character. The attributes
- * of each character includes its foreground and background color as well as whether
- * the character is underlined.  Each character is 8 pixels wide by 12 pixels high
- * which means that each column of characters is defined by the absolute pixel
- * horizontal position and each row of characters is defined by the absolute pixel
- * vertical position divided by 12.  The font ROM describes the shape of each
- * character as 12 "slices" of 8 bits. The characters are drawn slice by slice in
- * the rows. If a bit is "on," e.g. = 1, the foreground color is displayed else the
- * background color is displayed. If the underline attribute is set to 1, the 12th
- * slice is replaced by all foreground color. The cursor and character control logic
- * allows the cursor's row and column to be specified as well as the character code
- * to be written to the current cursor location. The cursor can be specified to be
- * either visible or not, and if visible, it can be specified to appear as a block
+ * of each character includes its foreground and background color as well as whether 
+ * the character is underlined.  Each character is 8 pixels wide by 12 pixels high 
+ * which means that each column of characters is defined by the absolute pixel 
+ * horizontal position and each row of characters is defined by the absolute pixel 
+ * vertical position divided by 12.  The font ROM describes the shape of each 
+ * character as 12 "slices" of 8 bits. The characters are drawn slice by slice in  
+ * the rows. If a bit is "on," e.g. = 1, the foreground color is displayed else the 
+ * background color is displayed. If the underline attribute is set to 1, the 12th 
+ * slice is replaced by all foreground color. The cursor and character control logic 
+ * allows the cursor's row and column to be specified as well as the character code 
+ * to be written to the current cursor location. The cursor can be specified to be 
+ * either visible or not, and if visible, it can be specified to appear as a block 
  * character or an underline. If the cursor is visible, the current character that
  * resides at the same location will be drawn as the cursor character.
  *
@@ -28,7 +28,7 @@
  * for this, the font ROM receives a horizontal value that is 2 less than the pixel's
  * actual position.  This will allow the characters to be correctly drawn, but they will
  * be drawn 2 pixel locations late.  In order to conpensate for this, the VGA controller's
- * horizongtal back porch is reduced by 2 so that the characters are drawn such that the
+ * horizongtal back porch is reduced by 2 so that the characters are drawn such that the 
  * current row begins at the beginning of the VGA display.
  *
  */
@@ -63,46 +63,54 @@ module vgachar(CLK_I, ck100, data, dstrobe, dtype, curvis, curblk, fgclr, bgclr,
     // 25MHz clock
     wire ck25;
     prescaler #(.N(2)) ps2(ck100, ck25);
+    
+    wire display_on;
+    wire [9:0] hpos;
+    wire [9:0] vpos;
 
-    // character code and attributes under cursor
-    reg [5:0] cursorRow;    // Row location of cursor
-    reg [6:0] cursorCol;    // Column location of cursor
-    reg [7:0] charrcReg;    // Character at cursor location
-    reg [24:0] attrrcReg;   // Attributes of char at cursor location
-    reg [32:0] wrChar;      // Char and attr of character to add to buffer
-    wire [32:0] rdChar;     // Char data from the buffer
-    wire display_on;        // Set if raster is in the 640x480 display area
-    wire [9:0] hpos;        // Horizontal location of raster
-    wire [9:0] vpos;        // Vertical location of raster
-    wire [3:0] yofs;        // y offset of the char to be displayed
-    wire [2:0] xofs;        // x offset of char to be displayed
-    wire [7:0] bits;        // horizontal 8-bit slice of the char to be displayed
-    wire [7:0] char;        // the char to be displayed is:
-                            // the cursor if current location matches cursor location
-                            // and cursor is visible, or the char from the display buffer
-    assign char = (((vpos/12) == cursorRow) && (hpos[9:3] == cursorCol && curvis)) ?
-                      ((curblk == 1) ? 8'hDB : 8'h5F) : rdChar[7:0];
-    assign yofs = vpos % 12; // index of the vertical slice of the char to be displayed
-    assign xofs = hpos[2:0] - 1;  // -1 to add time for buffer RAM read
-
-    // Instatiate the VGA sync generator
-    hvsync_generator hvsync_gen( .clk(ck25), .reset(0), .hsync(hsync), .vsync(vsync),
-        .display_on(display_on), .hpos(hpos), .vpos(vpos) );
-
-    // Instantiate the character buffer
-    display_buffer dispbuf( .wclk(CLK_I), .rclk(ck25), .we(dstrobe && (dtype == 0)),
-        .wr(cursorRow), .wc(cursorCol), .wd({underln, fgclr, bgclr, data}),
-        .rr(vpos/12), .rc(hpos[9:3]), .rd(rdChar) );
-
-    // Instantiate the font ROM
-    font437_rom font( .clk(ck25), .char(char), .yofs(yofs), .bits(bits) );
-
-
+    hvsync_generator hvsync_gen(
+        .clk(ck25),
+        .reset(0),
+        .hsync(hsync),
+        .vsync(vsync),
+        .display_on(display_on),
+        .hpos(hpos),
+        .vpos(vpos)
+    );
+       
+    // current row and column values
+`ifdef REAL_INIT    
+    reg [4:0] curRow = 0;
+    reg [6:0] curCol = 0;
+`else    
+    reg [5:0] curRow;
+    reg [6:0] curCol;
     initial begin
-        cursorRow = 19;
-        cursorCol = 40;
+        curRow = 19;
+        curCol = 40;
     end
+`endif    
+    
+    // character code and attributes under cursor
+    reg [7:0] charrcReg;
+    reg [24:0] attrrcReg;
 
+    // character code and attributes
+    reg [32:0] wrChar;
+    wire [32:0] rdChar;
+        
+    display_buffer dispbuf(
+        .wclk(CLK_I),
+        .rclk(ck25),
+        .we(dstrobe && (dtype == 0)),
+        .wr(curRow), 
+        .wc(curCol), 
+        .wd({underln, fgclr, bgclr, data}), 
+        // TODO: how to limit the number of bits in the result of the division?
+        .rr(vpos/12), 
+        .rc(hpos[9:3]), 
+        .rd(rdChar)
+    );
 
     // latch data defined by dtype using dstrobe on host clock (CLK_I) edge
     always @(posedge CLK_I)
@@ -112,14 +120,14 @@ module vgachar(CLK_I, ck100, data, dstrobe, dtype, curvis, curblk, fgclr, bgclr,
             if (dtype == 0)
             begin
                 // increment the cursor location
-                cursorCol <= (cursorCol < 79) ? cursorCol + 1 : 0;
-                if (cursorCol == 79)
-                    cursorRow <= (cursorRow < 39) ? cursorRow + 1 : 0;
+                curCol <= (curCol < 79) ? curCol + 1 : 0;
+                if (curCol == 79)
+                    curRow <= (curRow < 39) ? curRow + 1 : 0;
             end
             if (dtype == 1)
-                cursorCol <= data[6:0];
+                curCol <= data[6:0];
             else if (dtype == 2)
-                cursorRow <= data[5:0];
+                curRow <= data[5:0];
         end
     end
 
@@ -127,12 +135,33 @@ module vgachar(CLK_I, ck100, data, dstrobe, dtype, curvis, curblk, fgclr, bgclr,
     // latch the char and attributes under the cursor
     always@(posedge ck25)
     begin
-        if ((vpos/12) == cursorRow && hpos[9:3] == cursorCol) begin
+        if ((vpos/12) == curRow && hpos[9:3] == curCol) begin
             charrcReg <= rdChar[7:0];
             attrrcReg <= rdChar[32:8];
         end
     end
-
+    
+    // the char to be displayed is:
+    //   - the cursor if the current location coincides with the cursor location and the cursor is visible
+    //   - otherwise the char in the display buffer, i.e. the lower 8 bits of the indexed display buffer element
+    wire [7:0] char = ((vpos/12) == curRow && hpos[9:3] == curCol && curvis) ? ((curblk == 1) ? 8'hDB : 8'h5F) : rdChar[7:0];
+    
+    // index of the vertical slice of the char to be displayed
+    wire [3:0] yofs = vpos % 12;
+    
+    // index of the horizontal slice of the char to be displayed which
+    // is compensated for the 2 clocks it takes to read the character and its font
+    wire [2:0] xofs = hpos[2:0] - 2;
+  
+    // horizontal 8-bit slice of the char to be displayed
+    wire [7:0] bits;
+    
+    font437_rom font(
+        .clk(ck25),
+        .char(char),
+        .yofs(yofs),
+        .bits(bits)
+    );
 
     // the character is displayed as follows:
     //  - if a bit is 1 in the slice, output the fg color
@@ -143,8 +172,8 @@ module vgachar(CLK_I, ck100, data, dstrobe, dtype, curvis, curblk, fgclr, bgclr,
     assign blue  = (display_on) ? (bits[xofs ^ 3'b111] || (yofs == 10 && rdChar[32])) ? rdChar[23:20] : rdChar[11:8]  : 0;
 
     // outputs
-    assign currow = cursorRow;
-    assign curcol = cursorCol;
+    assign currow = curRow;
+    assign curcol = curCol;
     assign charrc = charrcReg;
     assign attrrc = attrrcReg;
 
