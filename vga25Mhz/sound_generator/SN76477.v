7e22b4ba56e390d2751f248cfd370565f2074aac
Jeff Westerinen <jefe@yahoo.com>
2023-05-01 19:07:37 -0700

    Added a second VCO and changed the switch.

--- vga25Mhz/sound_generator/SN76477.v
+++ vga25Mhz/sound_generator/SN76477.v
@@ -2,36 +2,45 @@
 `include "lfsr.v"
 
 /*
- * Project: sound_generator
+ * Module: SN76477
  *
- * This module has a square-wave oscillator (VCO) which can
- * be modulated by a low-frequency oscillator (LFO) and also
- * mixed with a LFSR noise source.
+ * This module is a modified SN76477 Complex Sound Generator device. It has two 
+ * square-wave oscillators (VCOs) which can be modulated by a low-frequency 
+ * oscillator (LFO) and also mixed with a LFSR noise source.
  *
  */
 
 module SN76477(clk, reset, spkr,
-               lfo_freq,noise_freq, vco_freq,
-               vco_select, noise_select, lfo_shift, mixer);
+               lfo_freq,noise_freq, 
+               vco1_freq, vco2_freq,
+               vco1_select, vco2_select, 
+               noise_select, lfo_shift, mixer);
 
     input clk, reset;
     output reg spkr = 0;		// module output
 
     input [9:0] lfo_freq;		// LFO frequency (10 bits)
     input [11:0] noise_freq;	// noise frequency (12 bits)
-    input [11:0] vco_freq;	    // VCO frequency (12 bits)
-    input vco_select;		    // 1 = LFO modulates VCO
+    
+    input [11:0] vco1_freq;	    // VCO frequency (12 bits)
+    input [11:0] vco2_freq;	    // VCO frequency (12 bits)
+    input vco1_select;		    // 1 = LFO modulates VCO
+    input vco2_select;		    // 1 = LFO modulates VCO
+    
     input noise_select;		    // 1 = LFO modulates Noise
     input [2:0] lfo_shift;	    // LFO modulation depth
-    input [2:0] mixer;		    // mix enable {LFO, Noise, VCO}
+    input [3:0] mixer;		    // mix enable {LFO, Noise, VCO2, VCO1}
 
     reg [3:0] div16;		    // divide-by-16 counter
     reg [17:0] lfo_count;		// LFO counter (18 bits)
     reg lfo_state;		        // LFO output
     reg [12:0] noise_count;	    // Noise counter (13 bits)
     reg noise_state;		    // Noise output
-    reg [12:0] vco_count;		// VCO counter (12 bits)
-    reg vco_state;		        // VCO output
+    
+    reg [12:0] vco1_count;		// VCO 1 counter (12 bits)
+    reg vco1_state;		        // VCO 1 output
+    reg [12:0] vco2_count;		// VCO 2 counter (12 bits)
+    reg vco2_state;		        // VCO 2 output
 
     wire [15:0] lfsr;		    // LFSR output
 
@@ -44,22 +53,33 @@ module SN76477(clk, reset, spkr,
 
     // create triangle waveform from LFO
     wire [11:0] lfo_triangle = lfo_count[17] ? ~lfo_count[17:6] : lfo_count[17:6];
-    wire [11:0] vco_delta = lfo_triangle >> lfo_shift;
+    wire [11:0] vco1_delta = lfo_triangle >> lfo_shift;
+    wire [11:0] vco2_delta = lfo_triangle >> lfo_shift;
 
     always @(posedge clk) begin
         // divide clock by 64
         div16 <= div16 + 1;
         if (div16 == 0) begin
         
-            // VCO oscillator
-            if (reset || vco_count == 0) begin
-                vco_state <= ~vco_state;
-                if (vco_select)
-                    vco_count <= vco_freq + vco_delta;
+            // VCO  1 oscillator
+            if (reset || vco1_count == 0) begin
+                vco1_state <= ~vco1_state;
+                if (vco1_select)
+                    vco1_count <= vco1_freq + vco1_delta;
                 else
-                    vco_count <= vco_freq + 0;
+                    vco1_count <= vco1_freq + 0;
             end else
-                vco_count <= vco_count - 1;
+                vco1_count <= vco1_count - 1;
+                
+            // VCO  2 oscillator
+            if (reset || vco2_count == 0) begin
+                vco2_state <= ~vco2_state;
+                if (vco2_select)
+                    vco2_count <= vco2_freq + vco2_delta;
+                else
+                    vco2_count <= vco2_freq + 0;
+            end else
+                vco2_count <= vco2_count - 1;
                 
             // LFO oscillator
             if (reset || lfo_count == 0) begin
@@ -73,14 +93,14 @@ module SN76477(clk, reset, spkr,
                 if (lfsr[0])
                     noise_state <= ~noise_state;
                 if (noise_select)
-                    noise_count <= noise_freq + vco_delta;
+                    noise_count <= noise_freq + vco1_delta;
                 else
                     noise_count <= noise_freq + 0;
             end else
                 noise_count <= noise_count - 1;
                 
             // Mixer            
-            spkr <= (lfo_state | ~mixer[2]) & (noise_state | ~mixer[1]) & (vco_state | ~mixer[0]);
+            spkr <= (lfo_state | ~mixer[3]) & (noise_state | ~mixer[2]) & (vco2_state | ~mixer[1]) & (vco1_state | ~mixer[0]);
         end
     end
 
--- vga25Mhz/sound_generator/sound_generator.v
+++ vga25Mhz/sound_generator/sound_generator.v
@@ -2,11 +2,21 @@
  * Project: sound_generator
  *
  * This project is the Basys3 port of the 8-bit Workshop "Sound Generator" project.
- * It is basically a Verilog interpretation of the SN76477 Complex Sound Generator
- * device.  In this project, the sound is output to connector JA1 so that a Pmod
- * AMP2 can be plugged into the JA connector to allow the low level sound to be
- * heard thru a speaker connected to the AMP2's output jack.  Switch sw0 controls
- * the gain.
+ * It is basically a Verilog interpretation of a modified SN76477 Complex Sound 
+ * Generator device.  In this project, the sound is output to connector JA1 so 
+ * that a Pmod AMP2 can be plugged into the JA connector to allow the low level 
+ * sound to be heard thru a speaker connected to the AMP2's output jack.  The 
+ * switches are grouped as follows:
+ *  sw2-0 controls the VCO1 frequency
+ *  sw5-3 controls the VCO2 frequency
+ *  sw7-6 controls the LFO frequency
+ *  sw8 is the VCO1 select (1 = modulated by the LFO)
+ *  sw9 is the VCO2 select (1 = modulated by the LFO)
+ *  sw10 is the noise select (1 = modulated by the LFO)
+ *  sw11 is the LFO modulation depth
+ *  sw15-12 controls the mixer (LFO, noise, VCO2, VCO1)
+ *
+ * Note: the noise frequency is fixed
  *
  */
 
@@ -16,7 +26,7 @@
 module sound_generator (
     input  clk,         // 100MHz clock
     input  btnC,        // reset button (center button on Basys3)
-    input [15:0] sw,     // SW0 to control gain 0=12dB, 1=6dB
+    input [15:0] sw,    // switches to control the inputs of the SN76477
     output JA1,         // Pmod AMP2 audio input
     output JA2,         // Pmod AMP2 gain
     output JA4          // Pmod AMP2 ~shutdown
@@ -30,13 +40,16 @@ module sound_generator (
         .clk(CLK_25MHz),
         .reset(btnC),
         .spkr(JA1),
-        .lfo_freq({sw[5],sw[4],sw[3],7'b0}),
-        .noise_freq({sw[8],sw[7],sw[6],5'b0}),
-        .vco_freq({sw[2],sw[1],sw[0],6'b0}),
-        .vco_select(sw[9]),
+        .lfo_freq({1'b1,sw[7],sw[6],7'b0}),
+        //.noise_freq({sw[8],sw[7],sw[6],5'b0}),
+        .noise_freq(90),
+        .vco1_freq({sw[2],sw[1],sw[0],6'b0}),
+        .vco2_freq({sw[5],sw[4],sw[3],6'b0}),
+        .vco1_select(sw[8]),
+        .vco2_select(sw[9]),
         .noise_select(sw[10]),
-        .lfo_shift({sw[12],sw[11]}),
-        .mixer({sw[15],sw[14],sw[13]})
+        .lfo_shift({0'b0,sw[11],1'b0}),
+        .mixer({sw[15],sw[14],sw[13],sw[12]})
     );
     
     assign JA2 = 0;
