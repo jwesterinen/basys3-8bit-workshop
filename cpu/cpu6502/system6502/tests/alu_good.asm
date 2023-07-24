eeca850a7581e85340afbf08203393a326f9e374
Jeff Westerinen <jefe@yahoo.com>
2023-07-24 15:13:09 -0700

    Added tests to show problems with the ALU.

---
+++ cpu/cpu6502/system6502/tests/alu_good.asm
@@ -0,0 +1,20 @@
+#include "iodefs.asm"
+
+op1 = $200
+op2 = $202
+
+LoadOps:
+    lda #$2
+    sta op1
+    lda #$1
+    sta op2
+    
+Begin:
+;    clc
+    lda op1
+    adc op2
+    sta LED_LSB
+
+End:
+    jmp End
+    
